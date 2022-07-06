12d0336a3369ffc57df88b137cb41832641a0006
Derrill05 <derrillkennoly@gmail.com>
2022-07-06 13:04:38 -0700

    fibonacci files

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,22 +9,18 @@
  */
 int main(void)
 {
-	int i;
-	long int a = 1;
-	long int b = 2;
+	int a = 1;
+	int b = 2;
+	long int sum = 0;
 
-	for (i = 0; i < 50; ++i)
-	{
-		printf("%ld", a);
-		if (i < 49)
-		{
-			b += a;
-			a = b - a;
-			printf(", ");
-		}
-	}
+	do {
+		if (b % 2 == 0)
+			sum += b;
+		b += a;
+		a = b - a;
+	} while (b <= 4000000);
 
-	putchar('\n');
+	printf("%ld\n", sum);
 
 	return (0);
 }
